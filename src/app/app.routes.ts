7652e23fda09a3f922354d3baf50a8f28b0b7bea
d1solis <d1solis@ucsd.edu>
2024-11-22 21:54:39 -0800

    Generated Contact Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
     {path:'home', component:HomeComponent},
     {path:'about', component:AboutComponent},
-    {path:'navbar', component:NavbarComponent}
+    {path:'navbar', component:NavbarComponent},
+    {path:'contact', component:ContactComponent}
 ];
